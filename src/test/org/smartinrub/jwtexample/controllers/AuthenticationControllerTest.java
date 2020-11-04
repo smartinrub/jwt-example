1f74b681b35c28617f47d040d22388424fd5b5dd
Sergio Martin Rubio <econsergio@gmail.com>
2020-11-05 00:18:04 +0100

    Fix test

--- src/test/org/smartinrub/jwtexample/controllers/AuthenticationControllerTest.java
+++ src/test/org/smartinrub/jwtexample/controllers/AuthenticationControllerTest.java
@@ -1,11 +1,11 @@
 package org.smartinrub.jwtexample.controllers;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.any;
@@ -13,22 +13,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest
-public class AuthenticationControllerTest {
+@ExtendWith(SpringExtension.class)
+class AuthenticationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void getTokenWhenInvalidUserShouldReturnBadRequest() throws Exception {
+    void getTokenWhenInvalidUserShouldReturnBadRequest() throws Exception {
 
         this.mockMvc.perform(post("/token").contentType(MediaType.APPLICATION_JSON)
                 .content("{\n\"email\": \"test\",\n\"password\": \"test\"\n}")).andExpect(status().isBadRequest());
     }
 
     @Test
-    public void getTokenWhenValidUserShouldReturnJwtToken() throws Exception {
+    void getTokenWhenValidUserShouldReturnJwtToken() throws Exception {
 
         this.mockMvc.perform(post("/token").contentType(MediaType.APPLICATION_JSON)
                 .content("{\n\"email\": \"test@gmail.com\",\n\"password\": \"test\"\n}")).andExpect(status().isOk())
--- src/test/org/smartinrub/jwtexample/controllers/WelcomeControllerTest.java
+++ src/test/org/smartinrub/jwtexample/controllers/WelcomeControllerTest.java
@@ -3,11 +3,11 @@ package org.smartinrub.jwtexample.controllers;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.ServletException;
@@ -15,32 +15,38 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest
-public class WelcomeControllerTest {
+@ExtendWith(SpringExtension.class)
+class WelcomeControllerTest {
 
     private static final String WELCOME_ENDPOINT = "/index";
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test(expected = ServletException.class)
-    public void welcomeWhenBearerTokenNotProvidedShouldThrowServletException() throws Exception {
-        mockMvc.perform(get(WELCOME_ENDPOINT));
-    }
+    @Test
+    void welcomeWhenBearerTokenNotProvidedShouldThrowServletException() {
+        assertThatThrownBy(() -> mockMvc.perform(get(WELCOME_ENDPOINT)))
+                .isInstanceOf(ServletException.class);
 
-    @Test(expected = JwtException.class)
-    public void welcomeWhenBearerTokenProvidedButInvalidShouldThrowJwtException() throws Exception {
-        mockMvc.perform(get(WELCOME_ENDPOINT).header("Authorization", "Bearer invalidjwttoken"));
     }
 
     @Test
-    public void welcomeWhenBearerTokenProvidedAndValidShouldReturnWelcomeMessage() throws Exception {
+    void welcomeWhenBearerTokenProvidedButInvalidShouldThrowJwtException() {
+        assertThatThrownBy(() -> mockMvc.perform(get(WELCOME_ENDPOINT)
+                .header("Authorization", "Bearer invalidjwttoken")))
+                .isInstanceOf(JwtException.class);
+
+    }
+
+    @Test
+    void welcomeWhenBearerTokenProvidedAndValidShouldReturnWelcomeMessage() throws Exception {
         String userName = "test";
         String email = "test@test.com";
 
@@ -50,7 +56,7 @@ public class WelcomeControllerTest {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + "30_000"))
+                .setExpiration(new Date(System.currentTimeMillis() + 30_000L))
                 .signWith(SignatureAlgorithm.HS256, "secret")
                 .compact();
 
