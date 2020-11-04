e3885b7df92d64e46d767b7bbfba48099316926e
Sergio Martin Rubio <econsergio@gmail.com>
2020-11-05 00:06:40 +0100

    Clean up

--- src/main/java/org/smartinrub/jwtexample/models/User.java
+++ src/main/java/org/smartinrub/jwtexample/models/User.java
@@ -4,28 +4,28 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 public class User {
-	
-	@NotEmpty
-	@Email
-	private String email;
 
-	@NotEmpty
-	private String password;
+    @NotEmpty
+    @Email
+    private String email;
 
-	public String getEmail() {
-		return email;
-	}
+    @NotEmpty
+    private String password;
 
-	public String getPassword() {
-		return password;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- src/main/java/org/smartinrub/jwtexample/utils/SecurityConstants.java
+++ src/main/java/org/smartinrub/jwtexample/utils/SecurityConstants.java
@@ -2,8 +2,8 @@ package org.smartinrub.jwtexample.utils;
 
 public class SecurityConstants {
 
-	public static final long JWT_EXPIRATION_TIME = 30_000;
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String HEADER_STRING = "Authorization";
+    public static final long JWT_EXPIRATION_TIME = 30_000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 
 }
